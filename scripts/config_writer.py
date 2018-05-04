3a6dada1ef37dac4d5fcde85426da3e27d602e5f
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2018-05-04 13:19:12 -0400

    config writer port fix

--- scripts/config_writer.py
+++ scripts/config_writer.py
@@ -8,5 +8,8 @@ def write_config(key, value):
 		with open(CONFIG_PATH, 'w') as conf:
 			conf.write("CONFIG_PATH='{}'\n".format(CONFIG_PATH))
 
+	if key=='PORT':
+		value = os.path.basename(value)
+
 	with open(CONFIG_PATH, 'a') as conf:
 		conf.write("{}='{}'\n".format(key, value))
